f1276176d485e9bd74ea379c35be2b98513640d9
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-04-18 14:28:49 +0300

    v_04

--- E_Commerce.MvcWebUI2/Entity/Category.cs
+++ E_Commerce.MvcWebUI2/Entity/Category.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +10,11 @@ namespace E_Commerce.MvcWebUI2.Entity
     public class Category
     {
         public int Id { get; set; }
+        [DisplayName("Kategori Adı")]
+        [StringLength(maximumLength:20,ErrorMessage ="En Fazla 20 karakter girilebilir..!")]
         public string Name { get; set; }
+        [DisplayName("Açıklama")]
+
         public string Description { get; set; }
         public List<Product> Products { get; set; }
     }
